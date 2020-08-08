331d2d7c643bddd7aaf3034eb0e37b05e88afbf5
GNULinuxIsGreat <GNULinuxIsGreat@users.noreply.github.com>
2020-08-08 14:45:24 +0200

    add doc

--- README.md
+++ README.md
@@ -4,8 +4,48 @@
 ![](https://tokei.rs/b1/github/GNULinuxIsGreat/bit_op)
 # Bit manipulation for rust
 ## Examples
+
+### Set bit that are selected
 ```rust
-//TODO
+use bit_op::BitOp;
+use bit_op::bit_u8::*;
+let mut x = 0b00001111u8;
+x.set(B7);
+assert_eq!(x, 0b10001111);
+let mut y = 0u8;
+y.set(B7 | B0);
+assert_eq!(y, 0b10000001);
+```
+### Reset bit that are selected
+```rust
+use bit_op::BitOp;
+use bit_op::bit_u8::*;
+let mut x = 0b00001111u8;
+x.reset(B0 | B1 | B2 | B3);
+assert_eq!(x, 0);
+let mut y = 0b11111111u8;
+y.reset(B7 | B0);
+assert_eq!(y, 0b01111110);
+```
+### Toggle bit that are selected
+```rust
+use bit_op::BitOp;
+use bit_op::bit_u8::*;
+let mut x = 0b00001111u8;
+x.toggle(B5 | B4 | B3 | B2);
+assert_eq!(x, 0b00110011);
+x.toggle(B5 | B4 | B3 | B2);
+assert_eq!(x, 0b00001111);
+```
+
+### Get the bits that are desired
+```rust
+use bit_op::BitOp;
+use bit_op::bit_u8::*;
+let mut x = 0b10000001u8;
+assert_eq!(x.get(B7), 0b10000000);
+assert_eq!(x.get(B6), 0b00000000);
+assert_eq!(x.get(B0), 0b00000001);
 ```
 ## License
 Licensed under either of Apache License, Version 2.0 or MIT license at your option.
--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,22 @@
 #![no_std]
 
-pub mod bit_u128;
-pub mod bit_u16;
-pub mod bit_u32;
-pub mod bit_u64;
+/// definitions of constants for each bit of u8
 pub mod bit_u8;
 
+/// definitions of constants for each bit of u16
+pub mod bit_u16;
+
+/// definitions of constants for each bit of u32
+pub mod bit_u32;
+
+/// definitions of constants for each bit of u64
+pub mod bit_u64;
+
+/// definitions of constants for each bit of u128
+pub mod bit_u128;
+
 use core::ops::{BitAnd, BitAndAssign, BitOrAssign, BitXorAssign, Not};
+/// provided the methods `set`, `reset`, `toggle`, `get`
 pub trait BitOp:
     Not<Output = Self>
     + BitOrAssign<Self>
@@ -15,18 +25,67 @@ pub trait BitOp:
     + BitAnd<Self, Output = Self>
     + Sized
 {
+    /**
+    # Set bit that are selected
+    ```
+    use bit_op::BitOp;
+    use bit_op::bit_u8::*;
+    let mut x = 0b00001111u8;
+    x.set(B7);
+    assert_eq!(x, 0b10001111);
+    let mut y = 0u8;
+    y.set(B7 | B0);
+    assert_eq!(y, 0b10000001);
+    ```
+    */
     fn set(&mut self, rhs: Self) {
         *self |= rhs;
     }
 
+        /**
+    # Reset bit that are selected
+    ```
+    use bit_op::BitOp;
+    use bit_op::bit_u8::*;
+    let mut x = 0b00001111u8;
+    x.reset(B0 | B1 | B2 | B3);
+    assert_eq!(x, 0);
+    let mut y = 0b11111111u8;
+    y.reset(B7 | B0);
+    assert_eq!(y, 0b01111110);
+    ```
+    */
     fn reset(&mut self, rhs: Self) {
         *self &= !rhs;
     }
 
+    /**
+    # Toggle bit that are selected
+    ```
+    use bit_op::BitOp;
+    use bit_op::bit_u8::*;
+    let mut x = 0b00001111u8;
+    x.toggle(B5 | B4 | B3 | B2);
+    assert_eq!(x, 0b00110011);
+    x.toggle(B5 | B4 | B3 | B2);
+    assert_eq!(x, 0b00001111);
+    ```
+    */
     fn toggle(&mut self, rhs: Self) {
         *self ^= rhs;
     }
 
+        /**
+    # Get the bits that are desired
+    ```
+    use bit_op::BitOp;
+    use bit_op::bit_u8::*;
+    let mut x = 0b10000001u8;
+    assert_eq!(x.get(B7), 0b10000000);
+    assert_eq!(x.get(B6), 0b00000000);
+    assert_eq!(x.get(B0), 0b00000001);
+    ```
+    */
     fn get(self, rhs: Self) -> Self {
         self & rhs
     }
@@ -37,6 +96,3 @@ impl BitOp for u16 {}
 impl BitOp for u32 {}
 impl BitOp for u64 {}
 impl BitOp for u128 {}
-
-#[cfg(test)]
-mod tests;
